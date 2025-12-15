44c11ec4c1022b40d823caee704010ed46cc4610
omarsholahbusiness <omarsholahbusiness@gmail.com>
2025-12-14 17:25:17 -0800

    v1.7

--- components/footer.tsx
+++ components/footer.tsx
@@ -30,7 +30,7 @@ export const Footer = () => {
             : ''
         }`}>
           <div className="inline-block bg-brand/10 border-2 border-brand/20 rounded-lg px-6 py-3 mb-4">
-            <p className="font-semibold text-lg text-brand"> واتساب : 01285949747</p>
+            <p className="font-semibold text-lg text-brand"> واتساب : 01112970189</p>
           </div>
           
           <p>© {new Date().getFullYear()} Mordesu Studio. جميع الحقوق محفوظة</p>
